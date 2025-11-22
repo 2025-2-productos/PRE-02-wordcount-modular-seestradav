e5b05d0f94dd392020842be081c9b06686a4732d
seestradav <seestradav@unal.edu.co>
2025-11-22 11:59:20 -0500

    hecho

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,55 +1,29 @@
 # obtain a list of files in the input directory
-import os
 
+import sys
+
+from ._internals.count_words import count_words
+from ._internals.preprocess_lines import preprocess_lines
+from ._internals.read_all_lines import read_all_lines
+from ._internals.split_into_words import split_into_words
 from ._internals.write_count_words import write_count_words
 
 
 def main():
-    ## read all lines
-    all_lines = []
-    input_files_list = os.listdir("data/input/")
-    for filename in input_files_list:
-        file_path = os.path.join("data/input", filename)
-        with open(file_path, "r", encoding="utf-8") as f:
-            lines = f.readlines()
-            all_lines.extend(lines)
 
-    ### preprocess lines
-    all_lines = [line.strip().lower() for line in all_lines]
+    if len(sys.argv) != 3:
+        print("Usage: python3 -m homework <input_folder> <output_folder>")
+        sys.exit(1)
 
-    ### split in words
-    words = []
-    for line in all_lines:
-        words.extend(words.strip(",.!?") for words in line.split())
+    input_folder = sys.argv[1]
+    output_folder = sys.argv[2]
 
-    ### count words
-    counter = {}
-    for word in words:
-        counter[word] = counter.get(word, 0) + 1
-
-    # count the frequency of the words in the files in the input directory
-    # counter = {}
-    # for filename in input_files_list:
-    #     with open("data/input/" + filename) as f:
-    #         for l in f:
-    #             for w in l.split():
-    #                 w = w.lower().strip(",.!?")
-    #                 counter[w] = counter.get(w, 0) + 1
-
-    write_count_words(counter)
+    all_lines = read_all_lines(input_folder)
+    all_lines = preprocess_lines(all_lines)
+    words = split_into_words(all_lines)
+    counter = count_words(words)
+    write_count_words(counter, output_folder)
 
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     main()
-
-
-# create the directory output/ if it doesn't exist
-# def write_count_words():
-#     if not os.path.exists("data/output"):
-#         os.makedirs("data/output")
-
-#     # save the results using tsv format
-#     with open("data/output/results.tsv", "w", encoding="utf-8") as f:
-#         for key, value in counter.items():
-#             # write the key and value to the file
-#             f.write(f"{key}\t{value}\n")
